1961706607d7cc1abae484d2dc8f496a05f6fec5
Muia23 <wilfred.muema@gmail.com>
2020-05-11 23:52:17 +0300

    Added what we do toggle

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+  $("#action1").click(function(){
+    $("#collapsible1").show();
+    $("#action1").hide();
+  });
+  $("#collapsible1").click(function(){
+    $("#action1").show();
+    $("#collapsible1").hide();
+  });
+
+  $("#action2").click(function(){
+    $("#collapsible2").show();
+    $("#action2").hide();
+  });
+  $("#collapsible2").click(function(){
+    $("#action2").show();
+    $("#collapsible2").hide();
+  });
+
+  $("#action3").click(function(){
+    $("#collapsible3").show();
+    $("#action3").hide();
+  });
+  $("#collapsible3").click(function(){
+    $("#action3").show();
+    $("#collapsible3").hide();
+  });
+
+});
